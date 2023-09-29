df2e9aa6a67bdb70c5df7a5d878f7026b6575220
FucaiChen <chenfucai9580@163.com>
2023-09-29 23:46:28 +0800

    add timer for speed test

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,32 @@
 #include "inc/polynomial.hpp"
 #include "inc/polynomial_root_finder.hpp"
 #include <iostream>
+
+#include <sys/time.h>
+
 using namespace std;
 
 void test_func() {
-  static constexpr int32_t N = 4;
-  std::array<double, N> coeff{0.0, 1.0, 2, 1.0};
-  math::polynomial::Polynomial1D<double, N> polynomial1(coeff.data());
-  math::polynomial::PolynomialRootFinder<double, N> solver;
+  typedef float D;
+  static constexpr int32_t N = 3;
+  std::array<D, N + 1> coeff{0.0, 2, -3, 1.0};
+  math::polynomial::Polynomial1D<D, N> polynomial1(coeff.data());
+  math::polynomial::PolynomialRootFinder<D, N> solver;
   std::int32_t roots_num;
-  std::array<double, N> real_roots;
-  solver.FindAllRealRoot(polynomial1, &roots_num, &real_roots[0]);
+  std::array<D, N> real_roots;
+
+  struct timeval t_begin, t_end;
+  static constexpr int32_t kLoop = 1000000;
+  gettimeofday(&t_begin,NULL);
+  for(int i = 0; i < kLoop; ++i) {
+    solver.FindAllRealRoot(polynomial1, &roots_num, &real_roots[0]);
+  }
+  gettimeofday(&t_end,NULL);
+
+  const double time_us = (t_end.tv_sec - t_begin.tv_sec) * 1e6 +
+  (t_end.tv_usec - t_begin.tv_usec);
+  std::cout << "time = " << time_us << " us"
+  << " av time = " << time_us / kLoop * 1e3 << " ns" << std::endl;
 
   std::cout << "poly roots num = " << roots_num << std::endl;
   for (int32_t i = 0 ; i < roots_num; ++i) {
@@ -21,6 +37,7 @@ void test_func() {
 int main(int argc, char **argv) {
 
   cout << "hello world!" << endl;
+
   test_func();
   return 0;
 }
